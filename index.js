76328f940996983cca8442c4eb825b2b3dc8e09b
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-12-06 21:24:07 +0530

    tried some changes

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+
+// function playSound(e) {
+//   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//   if(!audio) return;
+
+//   audio.currentTime = 0; 
+//   audio.play();
+
+//   key.classList.add('playing');
+// }
+
+// function removeTransition(e) {
+//   if(e.propertyName !== 'transform') return;
+//   this.classList.remove('playing');
+// }
+
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => {
+//   key.addEventListener('transitionend', removeTransition);
+
+//   const kbd = key.querySelector('kbd');
+//   const sound = key.querySelector('.sound');
+//   kbd.addEventListener('transitionend', removeTransition);
+//   sound.addEventListener('transitionend', removeTransition);
+// });
+
+// window.addEventListener('keydown', playSound);
+
+
 const keys = document.querySelectorAll('.key');
 const sounds = document.querySelectorAll('audio');
 
